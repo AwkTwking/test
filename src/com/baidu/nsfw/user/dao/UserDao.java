12cfa9e24c1b8f53cc2a5d519f34a4f15b06b33a
AwkTwking <13711377866@163.com>
2016-03-20 10:52:53 +0800

    add
    add

---
+++ src/com/baidu/nsfw/user/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.baidu.nsfw.user.dao;
+
+import com.baidu.core.dao.BaseDao;
+import com.baidu.nsfw.user.entity.User;
+
+public interface UserDao extends BaseDao<User> {
+	
+
+}
---
+++ src/com/baidu/nsfw/user/dao/impl/UserDaoImpl.java
@@ -0,0 +1,10 @@
+package com.baidu.nsfw.user.dao.impl;
+
+import com.baidu.core.dao.impl.BaseDaoImpl;
+import com.baidu.nsfw.user.dao.UserDao;
+import com.baidu.nsfw.user.entity.User;
+
+public class UserDaoImpl extends BaseDaoImpl<User> implements UserDao {
+
+	
+}
